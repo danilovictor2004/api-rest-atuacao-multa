e17fd7933d0e44e2a966b11a7e5d12ca71de2a6f
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-04-05 08:56:31 -0300

    :tada: commit initial

---
+++ src/main/java/com/algaworks/algatransito/AlgatransitoApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algatransito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgatransitoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgatransitoApiApplication.class, args);
+	}
+
+}
