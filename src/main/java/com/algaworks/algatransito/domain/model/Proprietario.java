80cb37902a01bd18d7177c49cc096dc2c8c401d2
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-04-05 10:38:29 -0300

    feat: configura o uso do lombok

--- src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
+++ src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
@@ -1,5 +1,10 @@
 package com.algaworks.algatransito.domain.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Proprietario {
 
     private Long id;
@@ -7,35 +12,4 @@ public class Proprietario {
     private String email;
     private String telefone;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
 }
