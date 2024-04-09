821b7eddad9d493cd215d38e02f170a369512791
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-04-09 15:18:51 -0300

    feat: Mapeia entidade proprietario com jakarta persistence

--- src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
+++ src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
@@ -1,15 +1,25 @@
 package com.algaworks.algatransito.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Proprietario {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
 
 }
