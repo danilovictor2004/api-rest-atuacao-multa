dfb3461a1ecb41b7489521c57635fd40c3856d58
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-04-10 09:20:13 -0300

    feat: implementa uma consulta na entidade Proprietario com JPQL

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,30 +1,22 @@
 package com.algaworks.algatransito.api.controller;
 
 import com.algaworks.algatransito.domain.model.Proprietario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ProprietarioController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        var proprietario1 = new Proprietario();
-        proprietario1.setId(1L);
-        proprietario1.setNome("Alvaro");
-        proprietario1.setTelefone("82 98888-7777");
-        proprietario1.setEmail("alvaro@email.com");
-
-        var proprietario2 = new Proprietario();
-        proprietario2.setId(2L);
-        proprietario2.setNome("Josué");
-        proprietario2.setTelefone("82 97777-7777");
-        proprietario2.setEmail("josue@email.com");
-
-        return Arrays.asList(proprietario1, proprietario2);
+        return manager.createQuery("from Proprietario", Proprietario.class).getResultList();
     }
 
 }
