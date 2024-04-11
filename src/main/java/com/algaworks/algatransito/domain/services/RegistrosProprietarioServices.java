d14fdad811b45060f462a8f2c9ffa7c0af7b5a98
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-04-11 14:16:17 -0300

    feat: Adiciona a anotação transaction

--- src/main/java/com/algaworks/algatransito/domain/services/RegistrosProprietarioServices.java
+++ src/main/java/com/algaworks/algatransito/domain/services/RegistrosProprietarioServices.java
@@ -4,6 +4,7 @@ import com.algaworks.algatransito.domain.model.Proprietario;
 import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
@@ -11,10 +12,12 @@ public class RegistrosProprietarioServices {
 
     private final ProprietarioRepository proprietarioRepository;
 
+    @Transactional
     public Proprietario salvar(Proprietario proprietario) {
         return proprietarioRepository.save(proprietario);
     }
 
+    @Transactional
     public void excluir(Long id) {
         proprietarioRepository.deleteById(id);
     }
