bae9861d926a7259797e8301c318e69c79f05a80
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-04-11 08:53:50 -0300

    feat: Implementa endpoint de exclusao de proprietario

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -3,7 +3,6 @@ package com.algaworks.algatransito.api.controller;
 import com.algaworks.algatransito.domain.model.Proprietario;
 import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -47,6 +46,17 @@ public class ProprietarioController {
         Proprietario proprietarioAtualizado = proprietarioRepository.save(proprietario);
         return ResponseEntity.ok(proprietarioAtualizado);
 
+    }
+
+    @DeleteMapping("/{proprietarioId}")
+    public ResponseEntity<Void> remover(@PathVariable Long proprietarioId) {
+        if (!proprietarioRepository.existsById(proprietarioId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        proprietarioRepository.deleteById(proprietarioId);
+
+        return ResponseEntity.noContent().build();
     }
 
 }
