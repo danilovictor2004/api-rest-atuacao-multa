37924618e6d674dc4c3dbe2feb7a23f17d3a5171
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-04-16 09:48:14 -0300

    feat: Cria e mapeia a entidade de veiculo

---
+++ src/main/java/com/algaworks/algatransito/domain/model/Veiculo.java
@@ -0,0 +1,34 @@
+package com.algaworks.algatransito.domain.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Veiculo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @ManyToOne
+    private Proprietario proprietario;
+
+    private String marca;
+    private String modelo;
+    private String placa;
+
+    @Enumerated(EnumType.STRING)
+    private StatusVeiculo status;
+
+    private LocalDateTime dataCadastro;
+    private LocalDateTime dataApreensao;
+
+}
