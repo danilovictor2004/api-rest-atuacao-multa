734f3a124c3afe836f36509942e957d75aa21b49
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-04-18 10:45:12 -0300

    feat: Usa a RFC 7807 (Problem Details for HTTP APIs)

--- src/main/java/com/algaworks/algatransito/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algatransito/exceptionhandler/ApiExceptionHandler.java
@@ -4,9 +4,10 @@ import com.algaworks.algatransito.domain.Exception.NegocioException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<String> capturar(NegocioException e) {
