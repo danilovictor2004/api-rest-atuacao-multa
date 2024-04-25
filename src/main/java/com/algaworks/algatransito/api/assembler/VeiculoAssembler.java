71690c8a99f1676406cdce3f2f51960bfa8ff0e3
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-04-25 10:22:50 -0300

    feat: Implementa assembler de Representation Model

---
+++ src/main/java/com/algaworks/algatransito/api/assembler/VeiculoAssembler.java
@@ -0,0 +1,24 @@
+package com.algaworks.algatransito.api.assembler;
+
+import com.algaworks.algatransito.api.model.VeiculoModel;
+import com.algaworks.algatransito.domain.model.Veiculo;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Component
+public class VeiculoAssembler {
+
+    private final ModelMapper modelMapper;
+
+    public VeiculoModel toModel(Veiculo veiculo) {
+        return modelMapper.map(veiculo, VeiculoModel.class);
+    }
+
+    public List<VeiculoModel> toCollectionModel(List<Veiculo> veiculos) {
+        return veiculos.stream().map(this::toModel).toList();
+    }
+}
--- src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
@@ -1,12 +1,12 @@
 package com.algaworks.algatransito.api.controller;
 
+import com.algaworks.algatransito.api.assembler.VeiculoAssembler;
 import com.algaworks.algatransito.api.model.VeiculoModel;
 import com.algaworks.algatransito.domain.model.Veiculo;
 import com.algaworks.algatransito.domain.repository.VeiculoRepository;
 import com.algaworks.algatransito.domain.services.RegistrosVeiculoServices;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,27 +20,25 @@ public class VeiculoController {
 
     private final VeiculoRepository veiculoRepository;
     private final RegistrosVeiculoServices registrosVeiculoServices;
-    private final ModelMapper modelMapper;
+    private final VeiculoAssembler veiculoAssembler;
 
     @GetMapping
     public List<VeiculoModel> listar() {
-        return veiculoRepository.findAll()
-                .stream()
-                .map(veiculo -> modelMapper.map(veiculo, VeiculoModel.class)).toList();
+        return veiculoAssembler.toCollectionModel(veiculoRepository.findAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<VeiculoModel> buscar(@PathVariable Long id) {
         return veiculoRepository.findById(id)
-                .map(veiculo -> modelMapper.map(veiculo, VeiculoModel.class))
+                .map(veiculoAssembler::toModel)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Veiculo cadastrar (@Valid @RequestBody Veiculo veiculo) {
-        return registrosVeiculoServices.cadastrar(veiculo);
+    public VeiculoModel cadastrar (@Valid @RequestBody Veiculo veiculo) {
+        return veiculoAssembler.toModel(registrosVeiculoServices.cadastrar(veiculo));
     }
 
 }
