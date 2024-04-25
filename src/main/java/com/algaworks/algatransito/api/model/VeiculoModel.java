30652639ae1d061cbf1e52018f34daf0541eafb6
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-04-25 10:28:11 -0300

    feat: Compondo objetos no representation model

--- src/main/java/com/algaworks/algatransito/api/model/VeiculoModel.java
+++ src/main/java/com/algaworks/algatransito/api/model/VeiculoModel.java
@@ -11,7 +11,7 @@ import java.time.OffsetDateTime;
 public class VeiculoModel {
 
     private Long id;
-    private String nomeProprietario;
+    private ProprietarioResumoModel proprietario;
     private String marca;
     private String modelo;
     private String placa;
