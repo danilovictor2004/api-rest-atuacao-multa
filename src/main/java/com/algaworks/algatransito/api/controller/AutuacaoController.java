9435d7ee5b36bcea61dd8f6209ee15b5508a368f
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-04-30 11:43:50 -0300

    feat: implementa o recurso de colecao de autuacoes

--- src/main/java/com/algaworks/algatransito/api/controller/AutuacaoController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/AutuacaoController.java
@@ -4,11 +4,15 @@ import com.algaworks.algatransito.api.assembler.AutuacaoAssembler;
 import com.algaworks.algatransito.api.model.AutuacaoModel;
 import com.algaworks.algatransito.api.model.input.AutuacaoInput;
 import com.algaworks.algatransito.domain.model.Autuacao;
+import com.algaworks.algatransito.domain.model.Veiculo;
 import com.algaworks.algatransito.domain.services.RegistroAutuacaoService;
+import com.algaworks.algatransito.domain.services.RegistrosVeiculoServices;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -17,6 +21,7 @@ public class AutuacaoController {
 
     private final AutuacaoAssembler autuacaoAssembler;
     private final RegistroAutuacaoService registroAutuacaoService;
+    private final RegistrosVeiculoServices registrosVeiculoServices;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -25,6 +30,12 @@ public class AutuacaoController {
         Autuacao novaAutuacao = autuacaoAssembler.toEntity(autuacaoInput);
         Autuacao autuacaoRegistrada = registroAutuacaoService.registrar(veiculoId, novaAutuacao);
         return autuacaoAssembler.toModel(autuacaoRegistrada);
+    }
+
+    @GetMapping
+    public List<AutuacaoModel> listar (@PathVariable Long veiculoId) {
+        Veiculo veiculo = registrosVeiculoServices.buscar(veiculoId);
+        return autuacaoAssembler.toCollectionModel(veiculo.getAutuacoes());
     }
 
 }
